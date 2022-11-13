131247bb78f5429b3f2e5da77e9c3d057efc685f
Hash2O <52819468+Hash2O@users.noreply.github.com>
2022-11-13 20:51:12 +0100

    First commit

---
+++ Assets/Challenge 1/Scripts/PlayerPropellerX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPropellerX : MonoBehaviour
+{
+    //private float propellerSpeed = 25.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(0, 0, Time.deltaTime);
+    }
+}
